c474ae86f64af30ea05e9b4461f211b95304af3c
Bui Cong <admtpu@gmail.com>
2020-08-31 09:19:18 +0700

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "remocolab.py",
     version = "0.1",
-    py_modules = ['auditcolab'],
+    py_modules = ['remocolab'],
     url = "https://github.com/macosupdate/auditcolab",
-    author = "buicong",
+    author = "demotomohiro",
 )
