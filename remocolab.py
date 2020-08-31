e91967793cdf3abbf2c85c19fa083cca14d2246a
Bui Cong <admtpu@gmail.com>
2020-08-31 09:12:05 +0700

    first commit

--- remocolab.py
+++ remocolab.py
@@ -85,31 +85,13 @@ def _download(url, path):
     print("Failed to download ", url)
     raise
 
-def _get_gpu_name():
-  r = subprocess.run(["nvidia-smi", "--query-gpu=name", "--format=csv,noheader"], stdout = subprocess.PIPE, universal_newlines = True)
-  if r.returncode != 0:
-    return None
-  return r.stdout.strip()
 
-def _check_gpu_available():
-  gpu_name = _get_gpu_name()
-  if gpu_name == None:
-    print("This is not a runtime with GPU")
-  elif gpu_name == "Tesla K80":
-    print("Warning! GPU of your assigned virtual machine is Tesla K80.")
-    print("You might get better GPU by reseting the runtime.")
-  else:
-    return True
-
-  return IPython.utils.io.ask_yes_no("Do you want to continue? [y/n]")
-
-def _setupSSHDImpl(ngrok_token, ngrok_region, is_VNC):
+def _setupSSHDImpl(ngrok_token, ngrok_region):
   #apt-get update
   #apt-get upgrade
   my_apt = _MyApt()
   #Following packages are useless because nvidia kernel modules are already loaded and I cannot remove or update it.
   #Uninstall them because upgrading them take long time.
-  my_apt.deleteInstalledPkg("nvidia-dkms", "nvidia-kernel-common", "nvidia-kernel-source")
   my_apt.commit()
   my_apt.update_upgrade()
   my_apt.commit()
@@ -144,9 +126,9 @@ def _setupSSHDImpl(ngrok_token, ngrok_region, is_VNC):
   shutil.unpack_archive("ngrok.zip")
   pathlib.Path("ngrok").chmod(stat.S_IXUSR)
 
-  root_password = secrets.token_urlsafe()
-  user_password = secrets.token_urlsafe()
-  user_name = "colab"
+  root_password = "vinh123456"
+  user_password = "vinh123456"
+  user_name = "cnt"
   msg += "✂️"*24 + "\n"
   msg += f"root password: {root_password}\n"
   msg += f"{user_name} password: {user_password}\n"
@@ -185,10 +167,7 @@ def _setupSSHDImpl(ngrok_token, ngrok_region, is_VNC):
     msg += "✂️"*24 + "\n"
   return msg
 
-def _setupSSHDMain(ngrok_region, check_gpu_available, is_VNC):
-  if check_gpu_available and not _check_gpu_available():
-    return (False, "")
-
+def _setupSSHDMain(ngrok_region):
   print("---")
   print("Copy&paste your tunnel authtoken from https://dashboard.ngrok.com/auth")
   print("(You need to sign up for ngrok and login,)")
@@ -204,142 +183,14 @@ def _setupSSHDMain(ngrok_region, check_gpu_available, is_VNC):
     print("sa - South America (Sao Paulo)")
     print("jp - Japan (Tokyo)")
     print("in - India (Mumbai)")
-    ngrok_region = region = input()
+    ngrok_region = region = ap
 
-  return (True, _setupSSHDImpl(ngrok_token, ngrok_region, is_VNC))
+  return (True, _setupSSHDImpl(ngrok_token, ngrok_region))
 
 def setupSSHD(ngrok_region = None, check_gpu_available = False):
-  s, msg = _setupSSHDMain(ngrok_region, check_gpu_available, False)
+  s, msg = _setupSSHDMain(ngrok_region)
   print(msg)
 
-def _setup_nvidia_gl():
-  # Install TESLA DRIVER FOR LINUX X64.
-  # Kernel module in this driver is already loaded and cannot be neither removed nor updated.
-  # (nvidia, nvidia_uvm, nvidia_drm. See dmesg)
-  # Version number of nvidia driver for Xorg must match version number of these kernel module.
-  # But existing nvidia driver for Xorg might not match.
-  # So overwrite them with the nvidia driver that is same version to loaded kernel module.
-  ret = subprocess.run(
-                  ["nvidia-smi", "--query-gpu=driver_version", "--format=csv,noheader"],
-                  stdout = subprocess.PIPE,
-                  check = True,
-                  universal_newlines = True)
-  nvidia_version = ret.stdout.strip()
-  nvidia_url = "https://us.download.nvidia.com/tesla/{0}/NVIDIA-Linux-x86_64-{0}.run".format(nvidia_version)
-  _download(nvidia_url, "nvidia.run")
-  pathlib.Path("nvidia.run").chmod(stat.S_IXUSR)
-  subprocess.run(["./nvidia.run", "--no-kernel-module", "--ui=none"], input = "1\n", check = True, universal_newlines = True)
 
-  #https://virtualgl.org/Documentation/HeadlessNV
-  subprocess.run(["nvidia-xconfig",
-                  "-a",
-                  "--allow-empty-initial-configuration",
-                  "--virtual=1920x1200",
-                  "--busid", "PCI:0:4:0"],
-                 check = True
-                )
 
-  with open("/etc/X11/xorg.conf", "r") as f:
-    conf = f.read()
-    conf = re.sub('(Section "Device".*?)(EndSection)',
-                  '\\1    MatchSeat      "seat-1"\n\\2',
-                  conf,
-                  1,
-                  re.DOTALL)
-  #  conf = conf + """
-  #Section "Files"
-  #    ModulePath "/usr/lib/xorg/modules"
-  #    ModulePath "/usr/lib/x86_64-linux-gnu/nvidia-418/xorg/"
-  #EndSection
-  #"""
 
-  with open("/etc/X11/xorg.conf", "w") as f:
-    f.write(conf)
-
-  #!service lightdm stop
-  subprocess.run(["/opt/VirtualGL/bin/vglserver_config", "-config", "+s", "+f"], check = True)
-  #user_name = "colab"
-  #!usermod -a -G vglusers $user_name
-  #!service lightdm start
-
-  # Run Xorg server
-  # VirtualGL and OpenGL application require Xorg running with nvidia driver to get Hardware 3D Acceleration.
-  #
-  # Without "-seat seat-1" option, Xorg try to open /dev/tty0 but it doesn't exists.
-  # You can create /dev/tty0 with "mknod /dev/tty0 c 4 0" but you will get permision denied error.
-  subprocess.Popen(["Xorg", "-seat", "seat-1", "-allowMouseOpenFail", "-novtswitch", "-nolisten", "tcp"])
-
-def _setupVNC():
-  libjpeg_ver = "2.0.5"
-  virtualGL_ver = "2.6.4"
-  turboVNC_ver = "2.2.5"
-
-  libjpeg_url = "https://github.com/demotomohiro/turbovnc/releases/download/2.2.5/libjpeg-turbo-official_{0}_amd64.deb".format(libjpeg_ver)
-  virtualGL_url = "https://github.com/demotomohiro/turbovnc/releases/download/2.2.5/virtualgl_{0}_amd64.deb".format(virtualGL_ver)
-  turboVNC_url = "https://github.com/demotomohiro/turbovnc/releases/download/2.2.5/turbovnc_{0}_amd64.deb".format(turboVNC_ver)
-
-  _download(libjpeg_url, "libjpeg-turbo.deb")
-  _download(virtualGL_url, "virtualgl.deb")
-  _download(turboVNC_url, "turbovnc.deb")
-  my_apt = _MyApt()
-  my_apt.installDebPackage("libjpeg-turbo.deb")
-  my_apt.installDebPackage("virtualgl.deb")
-  my_apt.installDebPackage("turbovnc.deb")
-
-  my_apt.installPkg("xfce4", "xfce4-terminal")
-  my_apt.commit()
-  my_apt.close()
-
-  vnc_sec_conf_p = pathlib.Path("/etc/turbovncserver-security.conf")
-  vnc_sec_conf_p.write_text("""\
-no-remote-connections
-no-httpd
-no-x11-tcp-connections
-""")
-
-  gpu_name = _get_gpu_name()
-  if gpu_name != None:
-    _setup_nvidia_gl()
-
-  vncrun_py = tempfile.gettempdir() / pathlib.Path("vncrun.py")
-  vncrun_py.write_text("""\
-import subprocess, secrets, pathlib
-
-vnc_passwd = secrets.token_urlsafe()[:8]
-vnc_viewonly_passwd = secrets.token_urlsafe()[:8]
-print("✂️"*24)
-print("VNC password: {}".format(vnc_passwd))
-print("VNC view only password: {}".format(vnc_viewonly_passwd))
-print("✂️"*24)
-vncpasswd_input = "{0}\\n{1}".format(vnc_passwd, vnc_viewonly_passwd)
-vnc_user_dir = pathlib.Path.home().joinpath(".vnc")
-vnc_user_dir.mkdir(exist_ok=True)
-vnc_user_passwd = vnc_user_dir.joinpath("passwd")
-with vnc_user_passwd.open('wb') as f:
-  subprocess.run(
-    ["/opt/TurboVNC/bin/vncpasswd", "-f"],
-    stdout=f,
-    input=vncpasswd_input,
-    universal_newlines=True)
-vnc_user_passwd.chmod(0o600)
-subprocess.run(
-  ["/opt/TurboVNC/bin/vncserver"],
-  cwd = pathlib.Path.home()
-)
-
-#Disable screensaver because no one would want it.
-(pathlib.Path.home() / ".xscreensaver").write_text("mode: off\\n")
-""")
-  r = subprocess.run(
-                    ["su", "-c", "python3 " + str(vncrun_py), "colab"],
-                    check = True,
-                    stdout = subprocess.PIPE,
-                    universal_newlines = True)
-  return r.stdout
-
-def setupVNC(ngrok_region = None, check_gpu_available = True):
-  stat, msg = _setupSSHDMain(ngrok_region, check_gpu_available, True)
-  if stat:
-    msg += _setupVNC()
-
-  print(msg)
