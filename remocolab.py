51f2493ccb459e8be539a60f548134b89f0434f9
Bui Cong <admtpu@gmail.com>
2020-09-01 09:33:30 +0700

    fix

--- remocolab.py
+++ remocolab.py
@@ -176,8 +176,8 @@ def _setupSSHDMain(ngrok_region,ngrok_auth):
 
   return (True, _setupSSHDImpl(ngrok_token, ngrok_region,ngrok_auth))
 
-def setupSSHD("ap", ngrok_auth):
-  s, msg = _setupSSHDMain(ngrok_region,ngrok_auth)
+def setupSSHD(ngrok_region, ngrok_auth):
+  s, msg = _setupSSHDMain("ap",ngrok_auth)
   print(msg)
 
 
